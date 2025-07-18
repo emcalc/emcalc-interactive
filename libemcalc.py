1fed7b38cf8166aa956eb8448208b0e16ec9167e
Eymn <eymenyildirim13@icloud.com>
2025-07-18 12:40:10 +0300

    libemcalc.py

---
+++ libemcalc.py
@@ -0,0 +1,21 @@
+def grams_to_kg(grams):
+    """Grams to Kilogram."""
+    return grams / 1000
+def calculate_theoretical_energy(mass_kg):
+    """calculating E=mc2 (%100 efficiency)."""
+    return mass_kg * const.c**2
+def calculate_practical_energy(theoretical_energy, efficiency):
+    """calculating pratical energy."""
+    return theoretical_energy * efficiency
+def convert_joules_to_electricity(practical_energy_joules, conversion_efficiency):
+    """calculating joules to watt energy."""
+    return practical_energy_joules * conversion_efficiency
+def calculate_led_on_time_seconds(electric_energy_joules, watt):
+    """calculating watt to seconds."""
+    return electric_energy_joules / watt
+if __name__ == "__main__":
+    import time
+    print("licansed under the GNU General Public License v3.0"
+          " (https://www.gnu.org/licenses/gpl-3.0.en.html)")
+    
+    exit()
