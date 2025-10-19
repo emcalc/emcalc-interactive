c8e5e341f22455cb2bad4a65c88a2447c2fa40da
Eymn <95621500+eymndev@users.noreply.github.com>
2025-10-19 15:50:16 +0300

    Bump version to 12.0.0 and update license format

--- setup.py
+++ setup.py
@@ -5,17 +5,15 @@ with open("requirements.txt") as f:
 
 setup(
     name='emcalc',
-    version='11.0.0',
+    version='12.0.0',
     python_requires=">=3.8,<4",
     author='eymndev',
     author_email='eymenyildirim13@icloud.com',
     description='Calculates E=mc^2, auto-converts gram to kilogram and returns energy in joules.',
     url='https://emcalc.github.io//',
-    license='Gnu Public License v3.0',
+    license='GNU Public License v3.0',
     install_requires=requirements,
     py_modules=[
-        'libemcalc',
-        'emcalc',
-        'CLI_emcalc'
+        'emcalc'
     ],
 )
